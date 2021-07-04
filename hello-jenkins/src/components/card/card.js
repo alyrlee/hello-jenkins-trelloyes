e465f754056a61161dc98f532f618775dafbb481
Ashley Lee <ashleyloreal.lee@gmail.com>
2021-07-04 11:23:04 -0400

    initial commit

---
+++ hello-jenkins/src/components/card/card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function Card(props) {
+    return (
+        <div className="card">
+            {props.text}
+        </div>
+    );
+};
+
+Card.defaultProps = {
+    text: ''
+};
